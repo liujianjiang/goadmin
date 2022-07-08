dadea4632de00d00224a9cc538afb208b4b64a3b
jianjiang.liu <jianjiang.liu@hand-china.com>
2022-07-08 14:08:43 +0800

    update'

---
+++ web/dist/js/gin-vue-admin-github-legacy.1657260112000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1657260112000 */
+System.register(["../gva/gin-vue-admin-index-legacy.1657260112000.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.A}],execute:function(){t({C:function(t){return i({url:"https://api.github.com/repos/liujianjiang/goadmin/commits?page="+t,method:"get"})},M:function(){return i({url:"https://api.github.com/orgs/FLIPPED-AURORA/members",method:"get"})}});var i=e.create()}}}));
---
+++ web/dist/js/gin-vue-admin-index-legacy.165726011200018.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1657260112000 */
+System.register(["../gva/gin-vue-admin-index-legacy.1657260112000.js"],(function(e){"use strict";var t,n,r,a;return{setters:[function(e){t=e.r,n=e.o,r=e.c,a=e.d}],execute:function(){var i={style:{height:"80vh"}},c=["src"],u={name:"FormGenerator"};e("default",Object.assign(u,{setup:function(e){var u=t("https://admin.liujianjiang.com"),o=t("8989");return function(e,t){return n(),r("div",i,[a("iframe",{width:"100%",height:"100%",src:"".concat(u.value,":").concat(o.value,"/form-generator/#/"),frameborder:"0"},null,8,c)])}}}))}}}));
